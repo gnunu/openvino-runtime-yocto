1188e1ecb2723d915d42911156448a1833c9f858
Shaoqiang <shaoqiang.chen@intel.com>
2018-08-30 19:04:39 +0800

    add versioning and cleanup logic

--- gen-runtime-openvino-fpga.sh
+++ gen-runtime-openvino-fpga.sh
@@ -1,35 +1,41 @@
 #!/bin/sh
 
-# this script will generate openvino runtime from l_openvino_toolkit_fpga_p_2018.2.300.tgz, i.e. openvino r2.
+# this script will generate openvino runtime from openvino toolkit
 
-# export BN=intel-cv-sdk-full-l-ipu-firmware-yocto-300-2018.0-300.noarch
+YEAR=2018
+VER=3
+REV=343
+YVR=${YEAR}.${VER}.${REV}
+RYR=${REV}-${YEAR}.0-${REV}
 
 # extract rpm files
 #echo -n extract rpm files ...
-#tar zxf l_openvino_toolkit_fpga_p_2018.2.300.tgz l_openvino_toolkit_fpga_p_2018.2.300/rpm --strip-components 1
+#tar zxf l_openvino_toolkit_fpga_p_${YVR}.tgz l_openvino_toolkit_fpga_p_${YVR}/rpm --strip-components 1
 #echo OK
 
+ROOT=`pwd`
+
 # define rootfs structure
-export USRBIN=rootfs/usr/bin
-export USRLIB=rootfs/usr/lib
-export LIB64=rootfs/lib64
-export SYSCONF=rootfs/etc
-export UDEVRULESD=${SYSCONF}/udev/rules.d
-export LDCONFD=${SYSCONF}/ld.so.conf.d
-export OPENVINO_CONF=${SYSCONF}/ld.so.conf.d/openvino.conf
-export OPENCLCONFD=${SYSCONF}/OpenCL/vendors
-export OPENVINO_LIB=rootfs/usr/lib/openvino/lib
-export OPENVINO_EXT=rootfs/usr/lib/openvino/external
-export OPENVINO_LIC=rootfs/usr/lib/openvino/licensing
-export OPENVINO_PYTHON=rootfs/usr/lib/openvino/python
-export OPENCV=rootfs/usr/lib/opencv
-export OPENCL=rootfs/usr/lib/opencl
-export OPENVX=rootfs/usr/lib/openvx
-export OPENVINO_RUNTIME_TGZ=openvino-runtime.tgz
+USRBIN=rootfs/usr/bin
+USRLIB=rootfs/usr/lib
+LIB64=rootfs/lib64
+SYSCONF=rootfs/etc
+UDEVRULESD=${SYSCONF}/udev/rules.d
+LDCONFD=${SYSCONF}/ld.so.conf.d
+OPENVINO_CONF=${SYSCONF}/ld.so.conf.d/openvino.conf
+OPENCLCONFD=${SYSCONF}/OpenCL/vendors
+OPENVINO_LIB=rootfs/usr/lib/openvino/lib
+OPENVINO_EXT=rootfs/usr/lib/openvino/external
+OPENVINO_LIC=rootfs/usr/lib/openvino/licensing
+OPENVINO_PYTHON=rootfs/usr/lib/openvino/python
+OPENCV=rootfs/usr/lib/opencv
+OPENCL=rootfs/usr/lib/opencl
+OPENVX=rootfs/usr/lib/openvx
+OPENVINO_RUNTIME_TGZ=openvino-runtime.tgz
 
 # set up rootfs structure
 echo -n set up rootfs structure ...
-mkdir -p ${OPENVINO_LIB} ${OPENVINO_EXT} ${OPENVINO_LIC} ${OPENVINO_PYTHON} ${OPENCV} ${OPENCL} ${OPENVX} ${USRBIN} ${SYSCONF} ${UDEVRULESD} ${LDCONFD} ${OPENCLCONFD} ${LIB64}
+mkdir -p ${OPENVINO_LIB} ${OPENVINO_EXT} ${OPENVINO_LIC} ${OPENVINO_PYTHON} ${OPENCV} ${OPENCL} ${OPENVX} ${USRBIN} ${SYSCONF} ${UDEVRULESD} ${LDCONFD} ${OPENCLCONFD}
 echo OK
 
 # license
@@ -37,48 +43,70 @@ cp -a ./licensing/* ${OPENVINO_LIC}
 
 # openvino IE
 echo -n install openvino IE libs ...
-rpm2cpio rpm/intel-cv-sdk-full-l-inference-engine-300-2018.0-300.noarch.rpm | cpio -id ./opt/intel/computer_vision_sdk_fpga_2018.2.300/deployment_tools/inference_engine/lib/ubuntu_16.04/intel64/* ./opt/intel/computer_vision_sdk_fpga_2018.2.300/deployment_tools/inference_engine/external/* ./opt/intel/computer_vision_sdk_fpga_2018.2.300/python/*
-mv ./opt/intel/computer_vision_sdk_fpga_2018.2.300/deployment_tools/inference_engine/lib/ubuntu_16.04/intel64/* ${OPENVINO_LIB}
-mv ./opt/intel/computer_vision_sdk_fpga_2018.2.300/deployment_tools/inference_engine/external/97-usbboot.rules ${UDEVRULESD}
-mv ./opt/intel/computer_vision_sdk_fpga_2018.2.300/deployment_tools/inference_engine/external/* ${OPENVINO_EXT}
-mv ./opt/intel/computer_vision_sdk_fpga_2018.2.300/python/* ${OPENVINO_PYTHON}
+rpm2cpio rpm/intel-cv-sdk-full-l-inference-engine-${RYR}.noarch.rpm | cpio -id ./opt/intel/computer_vision_sdk_fpga_${YVR}/deployment_tools/inference_engine/lib/ubuntu_16.04/intel64/* ./opt/intel/computer_vision_sdk_fpga_${YVR}/deployment_tools/inference_engine/external/* ./opt/intel/computer_vision_sdk_fpga_${YVR}/python/*
+mv ./opt/intel/computer_vision_sdk_fpga_${YVR}/deployment_tools/inference_engine/lib/ubuntu_16.04/intel64/* ${OPENVINO_LIB}
+mv ./opt/intel/computer_vision_sdk_fpga_${YVR}/deployment_tools/inference_engine/external/97-usbboot.rules ${UDEVRULESD}
+mv ./opt/intel/computer_vision_sdk_fpga_${YVR}/deployment_tools/inference_engine/external/* ${OPENVINO_EXT}
+if [ ${VER} = 2 ]; then
+    mv ./opt/intel/computer_vision_sdk_fpga_${YVR}/python/* ${OPENVINO_PYTHON}
+elif [ ${VER} = 3 ]; then
+# yocto refuses to accept more python versions dependancy
+    mkdir -p ${OPENVINO_PYTHON}/python2.7
+    mkdir -p ${OPENVINO_PYTHON}/python3.5
+    mv ./opt/intel/computer_vision_sdk_fpga_${YVR}/python/python2.7/ubuntu16/* ${OPENVINO_PYTHON}/python2.7
+    mv ./opt/intel/computer_vision_sdk_fpga_${YVR}/python/python3.5/ubuntu16/* ${OPENVINO_PYTHON}/python3.5
+fi
 echo OK
 
 # opencv
 echo -n install opencv libs ...
-rpm2cpio rpm/intel-cv-sdk-full-l-ocv-yocto-300-2018.0-300.noarch.rpm | cpio -id ./opt/intel/computer_vision_sdk_fpga_2018.2.300/opencv/lib/*
-mv ./opt/intel/computer_vision_sdk_fpga_2018.2.300/opencv/lib/* ${OPENCV}
+rpm2cpio rpm/intel-cv-sdk-full-l-ocv-yocto-${RYR}.noarch.rpm | cpio -id ./opt/intel/computer_vision_sdk_fpga_${YVR}/opencv/lib/*
+mv ./opt/intel/computer_vision_sdk_fpga_${YVR}/opencv/lib/* ${OPENCV}
 echo OK
 
 # openvx
 echo -n install openvx libs ...
-rpm2cpio rpm/intel-cv-sdk-full-l-ovx-rt-yocto-300-2018.0-300.noarch.rpm | cpio -id ./opt/intel/computer_vision_sdk_fpga_2018.2.300/openvx/lib/*
-mv ./opt/intel/computer_vision_sdk_fpga_2018.2.300/openvx/lib/* ${OPENVX}
+rpm2cpio rpm/intel-cv-sdk-full-l-ovx-rt-yocto-${RYR}.noarch.rpm | cpio -id ./opt/intel/computer_vision_sdk_fpga_${YVR}/openvx/lib/*
+mv ./opt/intel/computer_vision_sdk_fpga_${YVR}/openvx/lib/* ${OPENVX}
 echo OK
 
 # ipu firmware
 echo -n install ipu firmware for yocto ...
-rpm2cpio rpm/intel-cv-sdk-full-l-ipu-firmware-yocto-300-2018.0-300.noarch.rpm | cpio -id
+rpm2cpio rpm/intel-cv-sdk-full-l-ipu-firmware-yocto-${RYR}.noarch.rpm | cpio -id
 cd rootfs
-rpm2cpio ../opt/intel/computer_vision_sdk_fpga_2018.2.300/l_ipu_firmware_yocto/ipu4fw-cvsdk-r12018-20170225.rpm | cpio -id
-rpm2cpio ../opt/intel/computer_vision_sdk_fpga_2018.2.300/l_ipu_firmware_yocto/ipucompute-1.0.3-2018r1.x86_64.rpm | cpio -id
+rpm2cpio ../opt/intel/computer_vision_sdk_fpga_${YVR}/l_ipu_firmware_yocto/ipu4fw-cvsdk-r12018-20170225.rpm | cpio -id
+rpm2cpio ../opt/intel/computer_vision_sdk_fpga_${YVR}/l_ipu_firmware_yocto/ipucompute-1.0.3-2018r1.x86_64.rpm | cpio -id
 cd ..
 echo OK
 
 # fpga
 echo -n install FPGA necessities ...
-rpm2cpio rpm/intel-cv-sdk-full-l-opencl-rte-300-2018.0-300.noarch.rpm | cpio -id
+rpm2cpio rpm/intel-cv-sdk-full-l-opencl-rte-${RYR}.noarch.rpm | cpio -id
 mv ./aoclrte.run ${USRBIN}
-rpm2cpio rpm/aocl-pro-rte-17.1.2-304.x86_64.rpm | cpio -id
+if [ ${VER} = 2 ]; then
+    AOCL_PACK=aocl-pro-rte-17.1.2-304.x86_64.rpm
+elif [ ${VER} = 3 ]; then
+    AOCL_PACK=aocl-pro-rte-17.1.1-273p.x86_64.rpm
+fi
+rpm2cpio rpm/${AOCL_PACK} | cpio -id
 mv ./opt/altera ${USRLIB}
 echo OK
 
 # opencl
 echo -n install opencl libs ...
-rpm2cpio rpm/intel-cv-sdk-full-gfx-install-300-2018.0-300.noarch.rpm | cpio -id ./opt/intel/computer_vision_sdk_fpga_2018.2.300/install_dependencies/intel-opencl_2018ww15-010713_amd64.deb
-ar -x ./opt/intel/computer_vision_sdk_fpga_2018.2.300/install_dependencies/intel-opencl_2018ww15-010713_amd64.deb
+if [ ${VER} = 2 ]; then
+    OPENCL_PACK=intel-opencl_2018ww15-010713_amd64.deb
+elif [ ${VER} = 3 ]; then
+    OPENCL_PACK=intel-opencl_18.28.11080_amd64.deb
+fi
+rpm2cpio rpm/intel-cv-sdk-full-gfx-install-${RYR}.noarch.rpm | cpio -id ./opt/intel/computer_vision_sdk_fpga_${YVR}/install_dependencies/${OPENCL_PACK}
+ar -x ./opt/intel/computer_vision_sdk_fpga_${YVR}/install_dependencies/${OPENCL_PACK}
 tar xf data.tar.xz
+if [ ${VER} = 2 ]; then
 mv ./opt/intel/opencl/* ${OPENCL}
+elif [ ${VER} = 3 ]; then
+mv ./usr/local/lib/* ${OPENCL}
+fi
 echo OK
 
 # config files
@@ -112,5 +140,13 @@ echo -n packaing ...
 cd rootfs
 tar zcf ${OPENVINO_RUNTIME_TGZ} *
 echo OK
-echo The generated file is "${OPENVINO_RUNTIME_TGZ}"
+mv ${OPENVINO_RUNTIME_TGZ} ${ROOT}
+echo The generated file is "${ROOT}/${OPENVINO_RUNTIME_TGZ}"
+cd ${ROOT}
+
+# cleanup
+echo -n clean up ...
+rm -rf rootfs opt etc usr control.tar.gz data.tar.xz debian-binary _gpgorigin
+echo OK
+
 echo finished
